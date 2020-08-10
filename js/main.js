13990f9fbd168d234b095e872d02aabbf036ddcb
Natalia Bannikova <natabannikova10@gmail.com>
2020-08-10 16:48:31 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(function(){
 
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+    });
+
+});
+
+$('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
 });
